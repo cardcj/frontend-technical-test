7524f75f6e38077335d1878e8372813f8772dcdc
cardcj <cardwell.jamesc@gmail.com>
2021-09-24 14:26:37 +0100

    Task 1 complete

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,23 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  // Fetch the list of vehicles with summaries
+  const vehicleSummariesResponse = await fetch('/api/vehicles.json').then((response) => {
+    return response.json();
+  });
+  // Instantiate array for vehicles to be returned
+  const vehicleSummaryPayload = [];
+
+  vehicleSummariesResponse.forEach((vehicle) => {
+    // Get the details for the vehicle
+    fetch(vehicle.apiUrl).then((response) => {
+      return response.json();
+    }).then((vehicleDetails) => {
+      // Check that the vehicle has a valid price
+      if (vehicleDetails.price !== '' && vehicleDetails.price !== null) {
+        vehicleSummaryPayload.push(vehicle);
+      }
+    });
+  });
+  return vehicleSummaryPayload;
 }
--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -14,27 +14,20 @@ export default function VehicleList() {
     return <div data-testid="error">{ error }</div>;
   }
 
+  console.log(vehicles);
+
   return (
     <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
-      <p>
-        Visit
-        <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
-        {' '}
-        (main endpoint)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_fpace.json" target="_blank">/api/vehicle_fpace.json</a>
-        {' '}
-        (detail endpoint - apiUrl)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_xf.json" target="_blank">/api/vehicle_xf.json</a>
-        {' '}
-        (vehicle without any price)
-      </p>
+      {
+        vehicles.map((vehicle) => (
+            <li className="VehicleListing" key={vehicle.id}>
+              <img src={vehicle.media[0].url} alt={vehicle.media[0].name} />
+              <article>
+                <h1>{vehicle.id}</h1>
+              </article>
+            </li>
+          ))
+      }
     </div>
   );
 }
